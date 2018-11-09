23da2e9869c132dbb8bbc01cc6743edfb91c1eb5
fedor <fe.lap.prog@gmail.com>
2018-11-09 16:44:20 +0300

    correct modify AddVar command execute

--- vm_exec.cpp
+++ vm_exec.cpp
@@ -4,104 +4,106 @@
 #include <map>
 #include <fstream>
 #include <iostream>
+#include <cstdlib>
+
 
 using namespace std;
 
 
 enum{
-    TINT,
-    TCH,
-    TSTR,
-    TD,
-    TADDR,
-    TSTRUCT
+	TINT,
+	TCH,
+	TSTR,
+	TD,
+	TADDR,
+	TSTRUCT
 };
 
 class Var{
 private:
-    struct VarNode{
-        union{
-            char *sval;
-            char ch;
-            long long int ival;
-            long double dval;
+	struct VarNode{
+		union{
+			char *sval;
+			char ch;
+			long long int ival;
+			long double dval;
 
-            void *pval;
-        };
+			void *pval;
+		};
 
-        int kind_type;
-        string label;
-        bool is_localvar=false;
-    };
+		int kind_type;
+		string label;
+		bool is_localvar=false;
+	};
 
-    struct StructVal{
-        vector<struct VarNode> dictVarsStruct;
-        string labelstruct;
-        int id_var;
-        bool is_localvar=false;
-    };
+	struct StructVal{
+		vector<struct VarNode> dictVarsStruct;
+		string labelstruct;
+		int id_var;
+		bool is_localvar=false;
+	};
 
 
 public:
-    VarNode var;
-    struct StructVal varstruct;
-    int id_var;
-    bool operator ==(Var eq){
-        if(this->var.kind_type != eq.var.kind_type) return false;
-        switch(this->var.kind_type){
-            case TINT:{
+	VarNode var;
+	struct StructVal varstruct;
+	int id_var;
+	bool operator ==(Var eq){
+		if(this->var.kind_type != eq.var.kind_type) return false;
+		switch(this->var.kind_type){
+			case TINT:{
 
-                if(this->var.ival != eq.var.ival) return false;
-            }
-            case TADDR:{
-                if(this->var.pval != eq.var.pval) return false;
-            }
-            case TCH:{
-                if(this->var.ch != eq.var.ch) return false;
-            }
-            case TD:{
-                if(this->var.dval != eq.var.dval) return false;
-            }
-            case TSTR:{
-                if(strcmp(this->var.sval,eq.var.sval) != 0) return false;
-            }
-            case TSTRUCT:{
-                break;
-            }
+				if(this->var.ival != eq.var.ival) return false;
+			}
+			case TADDR:{
+				if(this->var.pval != eq.var.pval) return false;
+			}
+			case TCH:{
+				if(this->var.ch != eq.var.ch) return false;
+			}
+			case TD:{
+				if(this->var.dval != eq.var.dval) return false;
+			}
+			case TSTR:{
+				if(strcmp(this->var.sval,eq.var.sval) != 0) return false;
+			}
+			case TSTRUCT:{
+				break;
+			}
 
-        }
-        return true;
-    }
+		}
+		return true;
+	}
 };
 
 class Struct : private Var{
 public:
 
 
-    StructVal var;
+	StructVal var;
 
-    bool operator ==(Var eq){
-        if(this->var.kind_type != eq.var.kind_type) return false;
-        switch(this->var.kind_type){
-            case TINT:{
+	bool operator ==(Var eq){
+		if(this->var.kind_type != eq.var.kind_type) return false;
+		switch(this->var.kind_type){
+			case TINT:{
 
-                if(this->var.ival != eq.var.ival) return false;
-            }
-            case TADDR:{
-                if(this->var.pval != eq.var.pval) return false;
-            }
-            case TCH:{
-                if(this->var.ch != eq.var.ch) return false;
-            }
-            case TD:{
-                if(this->var.dval != eq.var.dval) return false;
-            }
-            case TSTR:{
-                if(strcmp(this->var.sval,eq.var.sval) != 0) return false;
-            }
-        }
-        return true;
-    }
+				if(this->var.ival != eq.var.ival) return false;
+			}
+			case TADDR:{
+				if(this->var.pval != eq.var.pval) return false;
+			}
+			case TCH:{
+				if(this->var.ch != eq.var.ch) return false;
+			}
+			case TD:{
+				if(this->var.dval != eq.var.dval) return false;
+			}
+			case TSTR:{
+				if(strcmp(this->var.sval,eq.var.sval) != 0) return false;
+			}
+		}
+		return true;
+	}
 };
 
 
@@ -112,235 +114,264 @@ std::map<string,Var> SysRegs;
 
 void init_sys(void){
 
-    SysRegs.insert(std::pair<string,Var>("ASave",{id_var : 0;var.kind_type : TADDR;var.ival=0;var.label : "ASave"}));
-    SysRegs.insert(std::pair<string,Var>("Isave",{id_var : 0;var.kind_type : TINT;var.ival=0;var.label : "Isave"}));
-    SysRegs.insert(std::pair<string,Var>("Ssave",{id_var : 0;var.kind_type : TSTR;var.ival=0;var.label : "Ssave"}));
-    SysRegs.insert(std::pair<string,Var>("Dsave",{id_var : 0;var.kind_type : TD;var.ival=0;var.label : "Dsave"}));
+	SysRegs.insert(std::pair<string,Var>("ASave",{id_var : 0;var.kind_type : TADDR;var.paddr=0x0;var.label : "ASave"}));
+	SysRegs.insert(std::pair<string,Var>("Isave",{id_var : 0;var.kind_type : TINT;var.ival=0;var.label : "Isave"}));
+	SysRegs.insert(std::pair<string,Var>("Ssave",{id_var : 0;var.kind_type : TSTR;var.sval=0;var.label : "Ssave"}));
+	SysRegs.insert(std::pair<string,Var>("Csave",{id_var : 0;var.kind_type : TCH;var.ch=0;var.label : "Csave"}));
+	SysRegs.insert(std::pair<string,Var>("Dsave",{id_var : 0;var.kind_type : TD;var.dval=0;var.label : "Dsave"}));
 }
 
 
 void RegVar(Var *tmp,bool is_local=false){
-    if(!is_local) mapVars.insert(mapVars.end(),*tmp);
-    else{
-        CurFuncVars.insert(CurFuncVars.end(),*tmp);
-        CurFuncVars[CurFuncVars.end()].var.is_localvar=true;
+	if(!is_local) mapVars.insert(mapVars.end(),*tmp);
+	else{
+		CurFuncVars.insert(CurFuncVars.end(),*tmp);
+		CurFuncVars[CurFuncVars.end()].var.is_localvar=true;
 
-        mapVars.insert(mapVars.end(),*tmp);
+		mapVars.insert(mapVars.end(),*tmp);
 
-        mapVars[mapVars.end()].var.is_localvar=true;
-        return mapVars
-    }
+		mapVars[mapVars.end()].var.is_localvar=true;
+		return mapVars
+	}
 
 }
 
 void DelVar(Var *find_var,bool is_local=true){
 
-    for (vector<Var>::iterator it = mapVars.begin(); it != mapVars.end(); it++) {
-        if(*it == find_var) mapVars[it].erase(it);
-    }
-    if(is_local){
-        for (vector<Var>::iterator it = CurFuncVars; it != CurFuncVars.end(); it++) {
-            if(*it == find_var) CurFuncVars[it].erase(it);
-        }
-    }
-    return;
+	for (vector<Var>::iterator it = mapVars.begin(); it != mapVars.end(); it++) {
+		if(*it == find_var) mapVars[it].erase(it);
+	}
+	if(is_local){
+		for (vector<Var>::iterator it = CurFuncVars; it != CurFuncVars.end(); it++) {
+			if(*it == find_var) CurFuncVars[it].erase(it);
+		}
+	}
+	return;
 }
 
 class OperatorFuncs{
 public:
 
-    void Sum(Var arg1,Var arg2,Var &argres){
-        if(arg1.var.kind_type == TDOUBLE)
-        {
-            if(arg1.var.kind_type == TINT) argres.var.dval=(long double)arg1.var.ival+arg1.var.dval;
-            if(arg1.var.kind_type == TDOUBLE) return;
-        }
-        if(arg1.var.kind_type == TINT)
-        {
-            if(arg1.var.kind_type == TINT) argres.var.ival=arg1.var.ival+(long long int)arg1.var.dval;
-            if(arg1.var.kind_type == TDOUBLE) return;
-        }
-        if(arg2.var.kind_type == TDOUBLE)
-        {
-            if(arg12var.kind_type == TINT) argres.var.dval=(long double)arg2.var.ival+arg2.var.dval;
-            if(arg2.var.kind_type == TDOUBLE) return;
-        }
-        if(arg2.var.kind_type == TINT)
-        {
-            if(arg2.var.kind_type == TINT) argres.var.ival=arg2.var.ival+(long long int)arg2.var.dval;
-            if(arg2.var.kind_type == TDOUBLE) return;
-        }
+	void Sum(Var arg1,Var arg2,Var &argres){
+		if(arg1.var.kind_type == TDOUBLE)
+		{
+			if(arg1.var.kind_type == TINT) argres.var.dval=(long double)arg1.var.ival+arg1.var.dval;
+			if(arg1.var.kind_type == TDOUBLE) return;
+		}
+		if(arg1.var.kind_type == TINT)
+		{
+			if(arg1.var.kind_type == TINT) argres.var.ival=arg1.var.ival+(long long int)arg1.var.dval;
+			if(arg1.var.kind_type == TDOUBLE) return;
+		}
+		if(arg2.var.kind_type == TDOUBLE)
+		{
+			if(arg12var.kind_type == TINT) argres.var.dval=(long double)arg2.var.ival+arg2.var.dval;
+			if(arg2.var.kind_type == TDOUBLE) return;
+		}
+		if(arg2.var.kind_type == TINT)
+		{
+			if(arg2.var.kind_type == TINT) argres.var.ival=arg2.var.ival+(long long int)arg2.var.dval;
+			if(arg2.var.kind_type == TDOUBLE) return;
+		}
 
-    }
-    void Sub(Var arg1,Var arg2,Var &argres){
-        if(arg1.var.kind_type == TDOUBLE)
-        {
-            if(arg1.var.kind_type == TINT) argres.var.dval=(long double)arg1.var.ival-arg1.var.dval;
-            if(arg1.var.kind_type == TDOUBLE) return;
-        }
-        if(arg1.var.kind_type == TINT)
-        {
-            if(arg1.var.kind_type == TINT) argres.var.ival=arg1.var.ival-(long long int)arg1.var.dval;
-            if(arg1.var.kind_type == TDOUBLE) return;
-        }
-        if(arg2.var.kind_type == TDOUBLE)
-        {
-            if(arg12var.kind_type == TINT) argres.var.dval=(long double)arg2.var.ival-arg2.var.dval;
-            if(arg2.var.kind_type == TDOUBLE) return;
-        }
-        if(arg2.var.kind_type == TINT)
-        {
-            if(arg2.var.kind_type == TINT) argres.var.ival=arg2.var.ival-(long long int)arg2.var.dval;
-            if(arg2.var.kind_type == TDOUBLE) return;
-        }
-    }
-    void Pow(Var arg1,Var arg2,Var argres){
-        if(arg1.var.kind_type == TDOUBLE)
-        {
-            if(arg1.var.kind_type == TINT) argres.var.dval=(long double)arg1.var.iva*arg1.var.dval;
-            if(arg1.var.kind_type == TDOUBLE) return;
-        }
-        if(arg1.var.kind_type == TINT)
-        {
-            if(arg1.var.kind_type == TINT) argres.var.ival=arg1.var.ival*(long long int)arg1.var.dval;
-            if(arg1.var.kind_type == TDOUBLE) return;
-        }
-        if(arg2.var.kind_type == TDOUBLE)
-        {
-            if(arg12var.kind_type == TINT) argres.var.dval=(long double)arg2.var.ival*arg2.var.dval;
-            if(arg2.var.kind_type == TDOUBLE) return;
-        }
-        if(arg2.var.kind_type == TINT)
-        {
-            if(arg2.var.kind_type == TINT) argres.var.ival=arg2.var.ival*(long long int)arg2.var.dval;
-            if(arg2.var.kind_type == TDOUBLE) return;
-        }
-    }
-    void Div(Var arg1,Var arg2,Var argres){
-        if(arg1.var.kind_type == TDOUBLE)
-        {
-            if(arg1.var.kind_type == TINT) argres.var.dval=(long double)arg1.var.ival/arg1.var.dval;
-            if(arg1.var.kind_type == TDOUBLE) return;
-        }
-        if(arg1.var.kind_type == TINT)
-        {
-            if(arg1.var.kind_type == TINT) argres.var.ival=arg1.var.ival/(long long int)arg1.var.dval;
-            if(arg1.var.kind_type == TDOUBLE) return;
-        }
-        if(arg2.var.kind_type == TDOUBLE)
-        {
-            if(arg12var.kind_type == TINT) argres.var.dval=(long double)arg2.var.ival/arg2.var.dval;
-            if(arg2.var.kind_type == TDOUBLE) return;
-        }
-        if(arg2.var.kind_type == TINT)
-        {
-            if(arg2.var.kind_type == TINT) argres.var.ival=arg2.var.ival/(long long int)arg2.var.dval;
-            if(arg2.var.kind_type == TDOUBLE) return;
-        }
-    }
+	}
+	void Sub(Var arg1,Var arg2,Var &argres){
+		if(arg1.var.kind_type == TDOUBLE)
+		{
+			if(arg1.var.kind_type == TINT) argres.var.dval=(long double)arg1.var.ival-arg1.var.dval;
+			if(arg1.var.kind_type == TDOUBLE) return;
+		}
+		if(arg1.var.kind_type == TINT)
+		{
+			if(arg1.var.kind_type == TINT) argres.var.ival=arg1.var.ival-(long long int)arg1.var.dval;
+			if(arg1.var.kind_type == TDOUBLE) return;
+		}
+		if(arg2.var.kind_type == TDOUBLE)
+		{
+			if(arg12var.kind_type == TINT) argres.var.dval=(long double)arg2.var.ival-arg2.var.dval;
+			if(arg2.var.kind_type == TDOUBLE) return;
+		}
+		if(arg2.var.kind_type == TINT)
+		{
+			if(arg2.var.kind_type == TINT) argres.var.ival=arg2.var.ival-(long long int)arg2.var.dval;
+			if(arg2.var.kind_type == TDOUBLE) return;
+		}
+	}
+	void Pow(Var arg1,Var arg2,Var argres){
+		if(arg1.var.kind_type == TDOUBLE)
+		{
+			if(arg1.var.kind_type == TINT) argres.var.dval=(long double)arg1.var.iva*arg1.var.dval;
+			if(arg1.var.kind_type == TDOUBLE) return;
+		}
+		if(arg1.var.kind_type == TINT)
+		{
+			if(arg1.var.kind_type == TINT) argres.var.ival=arg1.var.ival*(long long int)arg1.var.dval;
+			if(arg1.var.kind_type == TDOUBLE) return;
+		}
+		if(arg2.var.kind_type == TDOUBLE)
+		{
+			if(arg12var.kind_type == TINT) argres.var.dval=(long double)arg2.var.ival*arg2.var.dval;
+			if(arg2.var.kind_type == TDOUBLE) return;
+		}
+		if(arg2.var.kind_type == TINT)
+		{
+			if(arg2.var.kind_type == TINT) argres.var.ival=arg2.var.ival*(long long int)arg2.var.dval;
+			if(arg2.var.kind_type == TDOUBLE) return;
+		}
+	}
+	void Div(Var arg1,Var arg2,Var argres){
+		if(arg1.var.kind_type == TDOUBLE)
+		{
+			if(arg1.var.kind_type == TINT) argres.var.dval=(long double)arg1.var.ival/arg1.var.dval;
+			if(arg1.var.kind_type == TDOUBLE) return;
+		}
+		if(arg1.var.kind_type == TINT)
+		{
+			if(arg1.var.kind_type == TINT) argres.var.ival=arg1.var.ival/(long long int)arg1.var.dval;
+			if(arg1.var.kind_type == TDOUBLE) return;
+		}
+		if(arg2.var.kind_type == TDOUBLE)
+		{
+			if(arg12var.kind_type == TINT) argres.var.dval=(long double)arg2.var.ival/arg2.var.dval;
+			if(arg2.var.kind_type == TDOUBLE) return;
+		}
+		if(arg2.var.kind_type == TINT)
+		{
+			if(arg2.var.kind_type == TINT) argres.var.ival=arg2.var.ival/(long long int)arg2.var.dval;
+			if(arg2.var.kind_type == TDOUBLE) return;
+		}
+	}
 };
 
 bool add_loc_var=false;
 
 string CopyFromPos(string str,int pos){
-    string ret;
-    if(pos > str.size() || pos < 0) return "$$#error#$$";
-    for (size_t it2 = pos; it2 < str.size() ; it2++) {
-        ret.insert(ret.end(),str[it]);
-    }
-    return ret;
+	string ret;
+	if(pos > str.size() || pos < 0) return "$$#error#$$";
+	for (size_t it2 = pos; it2 < str.size() ; it2++) {
+		ret.insert(ret.end(),str[it]);
+	}
+	return ret;
 }
 
 int AnalyseCommand(string command , string arg1 , string arg2){
-    bool is_loc = add_loc_var;
-    if(command == "addvar"){ // add variable-command
-        Var newvar;
-        if(arg1 == "int"){
-            newvar.var.kind_type=TINT;
-            newvar.var.ival=0;
-            RegVar(newvar,is_loc);
-        }
-        if(arg1 == "double"){
-            newvar.var.kind_type=TD;
-            newvar.var.dval=0;
-            RegVar(newvar,is_loc);
-        }
-        if(arg1 == "char"){
-            newvar.var.kind_type=TCH;
-            newvar.var.ch=0;
-            RegVar(newvar,is_loc);
-        }
-        if(arg1 == "string"){
-            newvar.var.kind_type=TSTR;
-            newvar.var.sval=0;
-            RegVar(newvar,is_loc);
-        }
-        if(arg1 == "ptr"){
-            newvar.var.kind_type=TADDR;
-            newvar.var.pval=0x0;
-            RegVar(newvar,is_loc);
-        }
-    }
-    if(command == "add"){ // math + operator
-        if(arg1[0] == '$')
-        {
-            string num1=CopyFromPos(arg1,1);
-            if(arg2[0] == "$")
-            {
-                string num2=CopyFromPos(arg2,1);
-                map<string,Var>::iterator itreg=SysRegs.find("Dsave");
-                iteg->var->dval=stoi(num2,NULL);
-            }
-        }
-    }
-    if(command == "sub"){ // math - operator
+	bool is_loc = add_loc_var;
+	if(command == "addvar"){ // add variable-command
+		Var newvar;
+		if(arg1 == "int"){
+			newvar.var.kind_type=TINT;
+			newvar.var.ival=0;
+			RegVar(newvar,is_loc);
+		}
+		if(arg1 == "double"){
+			newvar.var.kind_type=TD;
+			newvar.var.dval=0;
+			RegVar(newvar,is_loc);
+		}
+		if(arg1 == "char"){
+			newvar.var.kind_type=TCH;
+			newvar.var.ch=0;
+			RegVar(newvar,is_loc);
+		}
+		if(arg1 == "string"){
+			newvar.var.kind_type=TSTR;
+			newvar.var.sval=0;
+			RegVar(newvar,is_loc);
+		}
+		if(arg1 == "ptr"){
+			newvar.var.kind_type=TADDR;
+			newvar.var.pval=0x0;
+			RegVar(newvar,is_loc);
+		}
+	}
+	if(command == "add"){ // math + operator
+		if(arg1[0] == '$')
+		{
+			string num1=CopyFromPos(arg1,1);
+			switch(arg1[1])
+			{
+				case '%':{ // float/double
+			   		string num2=CopyFromPos(arg1,2);
+					map<string,Var>::iterator itreg=SysRegs.find("Dsave");
+					iteg.value_type.var->dval=atof(num2.c_str());
+					break;
+				}
+				case '@':{ // int
+					string num2=CopyFromPos(arg1,2);
+					map<string,Var>::iterator itreg=SysRegs.find("Isave");
+					itreg.value_type.var->ival=atol(num2.c_str());
+					break;
+				}
+				case '$':{ // sym
+					string c2=CopyFromPos(arg1,2);
+					map<string,Var>::iterator itreg=SysRegs.find("Csave");
+					itreg.value_type.var->ch=c2[0];
+					break;
+				}
+				case '#':{ // string
+					string s2=CopyFromPos(arg1,2);
+					map<string,Var>::iterator itreg=SysRegs.find("Ssave");
+					itreg.value_type.var->pval=s2;	
+					break;
+				}
+				case '&':{
+					string c2=CopyFromPos(arg1,2);
+					map<string,Var>::iterator itreg=SysRegs.find("Asave");
+					itreg.value_type.var->pval=atol(c2.c_str());
+					break;
+				}
+			}
+		}
 
-    }
-    if(command == "pow"){ // math * operator
+	}
+	if(command == "sub"){ // math - operator
 
-    }
-    if(command == "div"){ // math / operator
+	}
+	if(command == "pow"){ // math * operator
 
-    }
-    if(command == "eq"){ // bin = operator
+	}
+	if(command == "div"){ // math / operator
 
-    }
-    if(command == "xor"){ // bin ^ operator
+	}
+	if(command == "eq"){ // bin = operator
 
-    }
-    if(command == "if"){ // logic if operator
+	}
+	if(command == "xor"){ // bin ^ operator
 
-    }
+	}
+	if(command == "if"){ // logic if operator
+
+	}
 }
 
 
 
 void Exec(string filename){
-    std::ifstream codefile(filename, std::ios::in);
-    int DEF_FUNC=100,DEF_VAR=200,ENDDEF_FUNC=1;
-    string arg1,arg2;
-    if(codefile.fail())
-    {
-        std::cerr << "Cannot to open file\n";
-    }
+	std::ifstream codefile(filename, std::ios::in);
+	int DEF_FUNC=100,DEF_VAR=200,ENDDEF_FUNC=1;
+	string arg1,arg2;
+	if(codefile.fail())
+	{
+		std::cerr << "Cannot to open file\n";
+	}
 
-    while(!codefile.eof()){
-        codefile >> command;
-        codefile.getline(arg1,256,',');
-        codefile.getline(arg2,256,',');
-        if(command == "newvar" && is_func) AnalyseCommand(command,arg1,arg2);
-        else{
-            int res=AnalyseCommand(command,arg1,arg2);
-            if(res == DEF_FUNC) add_loc_var=true;
-            if(res == ENDDEF_FUNC) add_loc_var=false;
-        }
+	while(!codefile.eof()){
+		codefile >> command;
+		codefile.getline(arg1,256,',');
+		codefile.getline(arg2,256,',');
+		if(command == "newvar" && is_func) AnalyseCommand(command,arg1,arg2);
+		else{
+			int res=AnalyseCommand(command,arg1,arg2);
+			if(res == DEF_FUNC) add_loc_var=true;
+			if(res == ENDDEF_FUNC) add_loc_var=false;
+		}
 
 
-    }
+	}
 }
 
 
 int main()
 {
-    cout << "hello world";
+	cout << "hello world";
 }
